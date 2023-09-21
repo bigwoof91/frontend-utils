6d6fac78ab5f1f718b745db1ea46ce6dfde05838
Michael Wolf <mike@mikesmbpm2max.mynetworksettings.com>
2023-09-21 16:23:27 -0400

    🧹 chore(repo): published packages

--- packages/browser/tsup.config.ts
+++ packages/browser/tsup.config.ts
@@ -2,11 +2,11 @@ import type { Options } from 'tsup';
 import { defineConfig } from 'tsup';
 
 export default defineConfig((options: Options) => ({
+  ...options,
   entry: ['src'],
   treeshake: true,
   splitting: true,
   format: ['esm', 'cjs'],
   minify: true,
   clean: true,
-  ...options,
 }));
