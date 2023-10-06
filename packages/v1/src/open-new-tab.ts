a03dcff79abfc208133a1c6e00602117d773f83d
Michael Wolf <mike@mikesmbpm2max.mynetworksettings.com>
2023-10-06 04:36:06 -0400

    fix(repo): remove package-lock.json

--- packages/v1/src/open-new-tab.ts
+++ packages/v1/src/open-new-tab.ts
@@ -1,4 +1,13 @@
-const openNewTab = (url: string) => window.open(url, '_blank');
+/**
+ * @todo add description/docs
+ * @todo add exceptions and logger
+ */
+const openNewTab = (url: string) => {
+  if (window?.open) {
+    window.open(url, '_blank');
+  }
+};
+
 openNewTab.curry = (url: string) => () => openNewTab(url);
 
 export { openNewTab };
--- packages/v1/src/window-redirect.ts
+++ packages/v1/src/window-redirect.ts
@@ -1,3 +1,7 @@
+/**
+ * @todo add description/docs
+ * @todo add exceptions and logger
+ */
 const windowRedirect = (url: string) => {
   if (window?.location) {
     window.location.assign(url);
