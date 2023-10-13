4b30d7dc776983e4dbe7efbd61ba3fd28e896ef1
Michael Wolf <mike@mikesmbpm2max.mynetworksettings.com>
2023-10-13 05:17:01 -0400

    chore(tests): make tests less verbose

--- packages/v1/src/__tests__/is-function.spec.ts
+++ packages/v1/src/__tests__/is-function.spec.ts
@@ -11,24 +11,13 @@ describe('isFunction', () => {
   });
 
   describe('when anything besides a function is passed', () => {
-    it('returns false', () => {
-      const res1 = isFunction(false);
-      const res2 = isFunction(true);
-      const res3 = isFunction('true');
-      const res4 = isFunction('');
-      const res5 = isFunction([]);
-      const res6 = isFunction({});
-      const res7 = isFunction(undefined);
-      const res8 = isFunction(null);
+    const falseyArgs = [false, true, 'true', '', [], {}, undefined, null];
 
-      expect(res1).toBe(false);
-      expect(res2).toBe(false);
-      expect(res3).toBe(false);
-      expect(res4).toBe(false);
-      expect(res5).toBe(false);
-      expect(res6).toBe(false);
-      expect(res7).toBe(false);
-      expect(res8).toBe(false);
-    });
+    for (const arg of falseyArgs) {
+      it('returns false', () => {
+        const result = isFunction(arg);
+        expect(result).toBe(false);
+      });
+    }
   });
 });
